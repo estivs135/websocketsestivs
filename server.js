0598776241b58943abe26b2594f94577c6b91d41
Estevan Santos <estevan.santos@gmail.com>
2024-07-24 17:13:06 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,59 @@
+/* eslint-env node */
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080, host: '0.0.0.0' });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  clients.push(ws);
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    console.log('Received:', data);
+
+    // Broadcast the message to all clients except the sender
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter((client) => client !== ws);
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on' );/* eslint-env node */
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080, host: '0.0.0.0' });
+
+let clients = [];
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  clients.push(ws);
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    console.log('Received:', data);
+
+    // Broadcast the message to all clients except the sender
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter((client) => client !== ws);
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server is running on' );
